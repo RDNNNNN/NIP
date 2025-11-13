aaedce81560358158205eb6c7411af99267e5ce9
RDNNNNN <z2715674@gmail.com>
2025-11-13 11:27:32 +0800

    update 20251103 class

---
+++ 20251103/README.md
@@ -0,0 +1,21 @@
+## Lighting and color spaces - Class 1
+
+![Class1_1](result/class1_1.png)
+
+![Class1_2](result/class1_2.png)
+
+![Class1_3](result/class1_3.png)
+
+![Class1_4](result/class1_4.png)
+
+## Thresholding - Class 2
+
+![Class2_1](result/class2_1.png)
+
+![Class2_2](result/class2_2.png)
+
+![Class2_3](result/class2_3.png)
+
+## Gradients and edge detection - Class 3
+
+![Class3](result/class3.png)
---
+++ 20251103/class2.py
@@ -0,0 +1,64 @@
+# --image license_plate.png
+# pip install scikit-image
+
+""" Page 39 """
+# import the necessary packages
+from skimage.filters import threshold_local
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, and blur it slightly
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imshow("Image", image)
+
+# apply Otsu's automatic thresholding -- Otsu's method automatically
+# determines the best threshold value `T` for us
+(T, threshInv) = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+cv2.imshow("Threshold", threshInv)
+print("Otsu's thresholding value: {}".format(T))
+
+# finally, we can visualize only the masked regions in the image
+cv2.imshow("Output", cv2.bitwise_and(image, image, mask=threshInv))
+cv2.waitKey(0)
+
+""" Page 11 """
+# apply basic thresholding -- the first parameter is the image
+# we want to threshold, the second value is is our threshold
+# check; if a pixel value is greater than our threshold (in this
+# case, 200), we set it to be BLACK, otherwise it is WHITE.
+(T, threshInv) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow("Threshold Binary Inverse", threshInv)
+
+# using normal thresholding (rather than inverse thresholding),
+# we can change the last argument in the function to make the coins
+# black rather than white.
+(T, thresh) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
+cv2.imshow("Threshold Binary", thresh)
+
+# finally, we can visualize only the masked regions in the image
+cv2.imshow("Output", cv2.bitwise_and(image, image, mask=threshInv))
+cv2.waitKey(0)
+
+""" Page 66 """
+# instead of manually specifying the threshold value, we can use adaptive
+# thresholding to examine neighborhoods of pixels and adaptively threshold
+# each neighborhood -- in this example, we'll calculate the mean value
+# of the neighborhood area of 25 pixels and threshold based on that value
+# finally, our constant C is subtracted from the mean calculation (in this case 15)
+thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+cv2.THRESH_BINARY_INV, 25, 15)
+cv2.imshow("OpenCV Mean Thresh", thresh)
+
+# personally, I prefer the scikit-image adaptive thresholding, it just
+# feels a lot more "Pythonic"
+T = threshold_local(blurred, 29, offset=5, method="gaussian")
+thresh = (blurred < T).astype("uint8") * 255
+cv2.imshow("scikit-image Mean Thresh", thresh)
+cv2.waitKey(0)
