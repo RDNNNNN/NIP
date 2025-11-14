eeff001e04f844a9dd3408d2c6c2dfca22a55801
RDNNNNN <z2715674@gmail.com>
2025-11-14 14:45:01 +0800

    update 20251027 class

--- 20251027/class3.py
+++ 20251027/class3.py
@@ -1,6 +1,7 @@
 # --image salt.png
 
-""" Page 11 """
+"""Page 11"""
+
 # import the necessary packages
 import argparse
 import cv2
@@ -16,10 +17,10 @@ cv2.imshow("Original", image)
 kernelSizes = [(3, 3), (9, 9), (15, 15)]
 
 # loop over the kernel sizes and apply an "average" blur to the image
-for (kX, kY) in kernelSizes:
+for kX, kY in kernelSizes:
     blurred = cv2.blur(image, (kX, kY))
     # cv2.imshow("Average ({}, {})".format(kX, kY), blurred)
-    cv2.imshow(f"Average({kX},{kY}",blurred)
+    cv2.imshow(f"Average({kX},{kY}", blurred)
     cv2.waitKey(0)
 
 """ Page 21 """
@@ -28,10 +29,10 @@ cv2.destroyAllWindows()
 cv2.imshow("Original", image)
 
 # loop over the kernel sizes and apply a "Gaussian" blur to the image
-for (kX, kY) in kernelSizes:
+for kX, kY in kernelSizes:
     blurred = cv2.GaussianBlur(image, (kX, kY), 0)
     # cv2.imshow("Gaussian ({}, {})".format(kX, kY), blurred)
-    cv2.imshow(f"Gaussian ({kX}, {kY})",blurred)
+    cv2.imshow(f"Gaussian ({kX}, {kY})", blurred)
     cv2.waitKey(0)
 
 """ Page 33 """
@@ -43,5 +44,5 @@ cv2.imshow("Original", image)
 for k in (3, 9, 15):
     blurred = cv2.medianBlur(image, k)
     # cv2.imshow("Median {}".format(k), blurred)
-    cv2.imshow(f"Median {k}",blurred)
+    cv2.imshow(f"Median {k}", blurred)
     cv2.waitKey(0)
